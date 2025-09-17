2ec896938c679219f90d7b0235e9ff3d60988f63
jordanxlj <jordanxlj@vip.sina.com>
2025-09-18 00:44:32 +0800

    modify the logging format.

--- tushare_provider/update_annual_report_ttm.py
+++ tushare_provider/update_annual_report_ttm.py
@@ -67,7 +67,7 @@ import concurrent.futures
 # Setup logging
 logging.basicConfig(
     level=logging.DEBUG,
-    format='%(asctime)s - %(levelname)s - %(message)s',
+    format='%(asctime)s - %(name)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s',
     handlers=[
         logging.StreamHandler(),
         logging.FileHandler('update_annual_report_ttm.log', mode='a')
