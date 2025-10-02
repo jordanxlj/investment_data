3630da4c34d7563855725f19214d525b556f6453
jordanxlj <jordanxlj@vip.sina.com>
2025-10-02 23:32:32 +0800

    fix dump_qlib_bin path bug.

--- dump_qlib_bin.sh
+++ dump_qlib_bin.sh
@@ -11,7 +11,7 @@ MYSQL_URL="mysql+pymysql://${MYSQL_USER}:@${MYSQL_HOST}:${MYSQL_PORT}/${MYSQL_DB
 QLIB_DATA="./temp_dir/qlib_source"
 NORMALIZE_DIR="./temp_dir/qlib_normalize"
 INDEX_DIR="./temp_dir/qlib_index"
-SCRIPT_DIR="./src/qlib"
+SCRIPT_DIR="./src"
 
 mkdir -p $QLIB_DATA
 python3 ./src/qlib/dump_all_to_qlib_source.py --mysql_url="${MYSQL_URL}" --output_dir=$QLIB_DATA
@@ -19,7 +19,7 @@ python3 ./src/qlib/dump_all_to_qlib_source.py --mysql_url="${MYSQL_URL}" --outpu
 export PYTHONPATH=`pwd`
 export PYTHONPATH=$PYTHONPATH:$WORKING_DIR/qlib_enhanced:$WORKING_DIR/qlib_enhanced/scripts
 
-python3 $SCRIPT_DIR/normalize.py normalize_data --source_dir $QLIB_DATA --normalize_dir $NORMALIZE_DIR --max_workers=16 --date_field_name="tradedate"
+python3 $SCRIPT_DIR/qlib/normalize.py normalize_data --source_dir $QLIB_DATA --normalize_dir $NORMALIZE_DIR --max_workers=16 --date_field_name="tradedate"
 python3 $WORKING_DIR/qlib_enhanced/scripts/dump_bin.py dump_all --csv_path $NORMALIZE_DIR --qlib_dir $WORKING_DIR/qlib_bin --date_field_name=tradedate --exclude_fields=tradedate,symbol
 
 mkdir -p $INDEX_DIR
